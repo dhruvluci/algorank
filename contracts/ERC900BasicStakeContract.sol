9b5fbb046d37276abcec10451d479866f39f4e44
dhruv <dhruv96@gmail.com>
2020-06-10 23:22:09 +0530

    staking

---
+++ contracts/ERC900BasicStakeContract.sol
@@ -0,0 +1,339 @@
+/* solium-disable security/no-block-members */
+// Staking aand unstaking $Ocean Tokens
+pragma solidity ^0.5.8;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/utils/Address.sol";
+//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol";
+
+import "./ERC900a.sol";
+import "./erc20a.sol";
+
+//contract ERC20Token {
+  //function transferFrom(address from, address to, uint value) public returns(bool);
+  //function balanceOf(address account) external view returns (uint256);
+//}
+
+/**
+ * @title ERC900 Simple Staking Interface basic implementation
+ * @dev See https://github.com/ethereum/EIPs/blob/master/EIPS/eip-900.md
+ */
+//pragma solidity ^0.6.0;
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+
+
+contract ERC900BasicStakeContract is ERC900 {
+  // @TODO: deploy this separately so we don't have to deploy it multiple times for each contract
+  using SafeMath for uint256;
+
+  // Token used for staking
+  IERC20 stakingToken = IERC20(address(0x985dd3D42De1e256d09e1c10F112bCCB8015AD41)) ;
+  //address payable addC = address(0x985dd3D42De1e256d09e1c10F112bCCB8015AD41);
+    //ERC20Token stakingToken = ERC20Token(addC);
+
+  
+  // The default duration of stake lock-in (in seconds)
+  uint256 public defaultLockInDuration;
+
+  // To save on gas, rather than create a separate mapping for totalStakedFor & personalStakes,
+  //  both data structures are stored in a single mapping for a given addresses.
+  //
+  // It's possible to have a non-existing personalStakes, but have tokens in totalStakedFor
+  //  if other users are staking on behalf of a given address.
+  mapping (address => StakeContract) public stakeHolders;
+
+  // Struct for personal stakes (i.e., stakes made by this address)
+  // unlockedTimestamp - when the stake unlocks (in seconds since Unix epoch)
+  // actualAmount - the amount of tokens in the stake
+  // stakedFor - the address the stake was staked for
+  struct Stake {
+    uint256 unlockedTimestamp;
+    uint256 actualAmount;
+    address stakedFor;
+  }
+
+  // Struct for all stake metadata at a particular address
+  // totalStakedFor - the number of tokens staked for this address
+  // personalStakeIndex - the index in the personalStakes array.
+  // personalStakes - append only array of stakes made by this address
+  // exists - whether or not there are stakes that involve this address
+  struct StakeContract {
+    uint256 totalStakedFor;
+
+    uint256 personalStakeIndex;
+
+    Stake[] personalStakes;
+
+    bool exists;
+  }
+
+  /**
+   * @dev Modifier that checks that this contract can transfer tokens from the
+   *  balance in the stakingToken contract for the given address.
+   * @dev This modifier also transfers the tokens.
+   * @param _address address to transfer tokens from
+   * @param _amount uint256 the number of tokens
+   * 
+   * 
+   */
+   //function get
+  //  uint256 bal = stakingToken.balanceOf(0xc966Ba2a41888B6B4c5273323075B98E27B9F364);
+    modifier canStake(address _address, uint256 _amount) {
+    require(
+        stakingToken.transferFrom(address(_address),address(this), _amount),
+      //stakingToken.transferFrom(_address, this, _amount),
+      "Stake required");
+
+    _;
+    }
+  //modifier canStake(address _address, uint256 _amount) {
+    //require(
+            //stakingToken.approve(address(this), _amount),
+      //stakingToken.transferFrom(_address, this, _amount),
+      //"Stake required");
+
+    //_;
+  //}
+
+  /**
+   * @dev Constructor function
+   * @param _stakingToken ERC20 The address of the token contract used for staking
+   */
+  constructor(IERC20 _stakingToken) public {
+   IERC20 stakingToken = IERC20(_stakingToken);
+  }
+
+  /**
+   * @dev Returns the timestamps for when active personal stakes for an address will unlock
+   * @dev These accessors functions are needed until https://github.com/ethereum/web3.js/issues/1241 is solved
+   * @param _address address that created the stakes
+   * @return uint256[] array of timestamps
+   */
+  function getPersonalStakeUnlockedTimestamps(address _address) external view returns (uint256[] memory) {
+    uint256[] memory timestamps;
+    (timestamps,,) = getPersonalStakes(_address);
+
+    return timestamps;
+  }
+
+  /**
+   * @dev Returns the stake actualAmount for active personal stakes for an address
+   * @dev These accessors functions are needed until https://github.com/ethereum/web3.js/issues/1241 is solved
+   * @param _address address that created the stakes
+   * @return uint256[] array of actualAmounts
+   */
+  function getPersonalStakeActualAmounts(address _address) external view returns (uint256[] memory) {
+    uint256[] memory actualAmounts;
+    (,actualAmounts,) = getPersonalStakes(_address);
+
+    return actualAmounts;
+  }
+
+  /**
+   * @dev Returns the addresses that each personal stake was created for by an address
+   * @dev These accessors functions are needed until https://github.com/ethereum/web3.js/issues/1241 is solved
+   * @param _address address that created the stakes
+   * @return address[] array of amounts
+   */
+  function getPersonalStakeForAddresses(address _address) external view returns (address[] memory) {
+    address[] memory stakedFor;
+    (,,stakedFor) = getPersonalStakes(_address);
+
+    return stakedFor;
+  }
+
+  /**
+   * @notice Stakes a certain amount of tokens, this MUST transfer the given amount from the user
+   * @notice MUST trigger Staked event
+   * @param _amount uint256 the amount of tokens to stake
+   * @param _data bytes optional data to include in the Stake event
+   */
+  function stake(uint256 _amount, bytes memory _data) public {
+    createStake(
+      msg.sender,
+      _amount,
+      defaultLockInDuration,
+      _data);
+  }
+
+  /**
+   * @notice Stakes a certain amount of tokens, this MUST transfer the given amount from the caller
+   * @notice MUST trigger Staked event
+   * @param _user address the address the tokens are staked for
+   * @param _amount uint256 the amount of tokens to stake
+   * @param _data bytes optional data to include in the Stake event
+   */
+  function stakeFor(address _user, uint256 _amount, bytes memory _data) public {
+    createStake(
+      _user,
+      _amount,
+      defaultLockInDuration,
+      _data);
+  }
+
+  /**
+   * @notice Unstakes a certain amount of tokens, this SHOULD return the given amount of tokens to the user, if unstaking is currently not possible the function MUST revert
+   * @notice MUST trigger Unstaked event
+   * @dev Unstaking tokens is an atomic operation—either all of the tokens in a stake, or none of the tokens.
+   * @dev Users can only unstake a single stake at a time, it is must be their oldest active stake. Upon releasing that stake, the tokens will be
+   *  transferred back to their account, and their personalStakeIndex will increment to the next active stake.
+   * @param _amount uint256 the amount of tokens to unstake
+   * @param _data bytes optional data to include in the Unstake event
+   */
+  function unstake(uint256 _amount, bytes memory _data) public {
+    withdrawStake(
+      _amount,
+      _data);
+  }
+
+  /**
+   * @notice Returns the current total of tokens staked for an address
+   * @param _address address The address to query
+   * @return uint256 The number of tokens staked for the given address
+   */
+  function totalStakedFor(address _address) public view returns (uint256) {
+      //return stakingToken.balanceOf(address(_address));
+    return stakeHolders[_address].totalStakedFor;
+  }
+
+  /**
+   * @notice Returns the current total of tokens staked
+   * @return uint256 The number of tokens staked in the contract
+   */
+  function totalStaked() public view returns (uint256) {
+    return stakingToken.balanceOf(address(this));
+  }
+
+  /**
+   * @notice Address of the token being used by the staking interface
+   * @return address The address of the ERC20 token used for staking
+   */
+  function token() public view returns (address) {
+    return address(stakingToken);
+  }
+
+  /**
+   * @notice MUST return true if the optional history functions are implemented, otherwise false
+   * @dev Since we don't implement the optional interface, this always returns false
+   * @return bool Whether or not the optional history functions are implemented
+   */
+  function supportsHistory() public pure returns (bool) {
+    return false;
+  }
+
+  /**
+   * @dev Helper function to get specific properties of all of the personal stakes created by an address
+   * @param _address address The address to query
+   * @return (uint256[], uint256[], address[])
+   *  timestamps array, actualAmounts array, stakedFor array
+   */
+  function getPersonalStakes(
+    address _address
+  )
+    view
+    public
+    returns(uint256[] memory, uint256[] memory, address[] memory)
+  {
+    StakeContract storage stakeContract = stakeHolders[_address];
+
+    uint256 arraySize = stakeContract.personalStakes.length - stakeContract.personalStakeIndex;
+    uint256[] memory unlockedTimestamps = new uint256[](arraySize);
+    uint256[] memory actualAmounts = new uint256[](arraySize);
+    address[] memory stakedFor = new address[](arraySize);
+
+    for (uint256 i = stakeContract.personalStakeIndex; i < stakeContract.personalStakes.length; i++) {
+      uint256 index = i - stakeContract.personalStakeIndex;
+      unlockedTimestamps[index] = stakeContract.personalStakes[i].unlockedTimestamp;
+      actualAmounts[index] = stakeContract.personalStakes[i].actualAmount;
+      stakedFor[index] = stakeContract.personalStakes[i].stakedFor;
+    }
+
+    return (
+      unlockedTimestamps,
+      actualAmounts,
+      stakedFor
+    );
+  }
+
+  /**
+   * @dev Helper function to create stakes for a given address
+   * @param _address address The address the stake is being created for
+   * @param _amount uint256 The number of tokens being staked
+   * @param _lockInDuration uint256 The duration to lock the tokens for
+   * @param _data bytes optional data to include in the Stake event
+   */
+  function createStake(
+    address _address,
+    uint256 _amount,
+    uint256 _lockInDuration,
+    bytes memory _data
+  )
+    internal
+    canStake(msg.sender, _amount)
+  {
+    if (!stakeHolders[msg.sender].exists) {
+      stakeHolders[msg.sender].exists = true;
+    }
+
+    stakeHolders[_address].totalStakedFor = stakeHolders[_address].totalStakedFor.add(_amount);
+    stakeHolders[msg.sender].personalStakes.push(
+      Stake(
+        block.timestamp.add(_lockInDuration),
+        _amount,
+        _address)
+      );
+
+    emit Staked(
+      _address,
+      _amount,
+      totalStakedFor(_address),
+      _data);
+  }
+
+  /**
+   * @dev Helper function to withdraw stakes for the msg.sender
+   * @param _amount uint256 The amount to withdraw. MUST match the stake amount for the
+   *  stake at personalStakeIndex.
+   * @param _data bytes optional data to include in the Unstake event
+   */
+  
+  function withdrawStake(
+    uint256 _amount,
+    bytes memory _data
+  )
+    internal
+  {
+    Stake storage personalStake = stakeHolders[msg.sender].personalStakes[stakeHolders[msg.sender].personalStakeIndex];
+
+    // Check that the current stake has unlocked & matches the unstake amount
+    require(
+      personalStake.unlockedTimestamp <= block.timestamp,
+      "The current stake hasn't unlocked yet");
+
+    require(
+      personalStake.actualAmount == _amount,
+      "The unstake amount does not match the current stake");
+
+    // Transfer the staked tokens from this contract back to the sender
+    // Notice that we are using transfer instead of transferFrom here, so
+    //  no approval is needed beforehand.
+    
+      stakingToken.transfer(address(msg.sender), _amount);
+    
+
+    stakeHolders[personalStake.stakedFor].totalStakedFor = stakeHolders[personalStake.stakedFor]
+      .totalStakedFor.sub(personalStake.actualAmount);
+
+    personalStake.actualAmount = 0;
+    stakeHolders[msg.sender].personalStakeIndex++;
+
+    emit Unstaked(
+      personalStake.stakedFor,
+      _amount,
+      totalStakedFor(personalStake.stakedFor),
+      _data);
+  }
+}
